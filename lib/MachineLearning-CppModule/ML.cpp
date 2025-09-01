0cefaad076ada31ae4a36da35d559cf098c444cc
Boxik-CZ <petrzelinka8@gmail.com>
2025-09-01 15:42:55 +0200

    added js support for ml

--- lib/MachineLearning-CppModule/ML.cpp
+++ lib/MachineLearning-CppModule/ML.cpp
@@ -1,126 +1,130 @@
 #include <vector>
 #include <cmath>
 #include "ML.h"
+#include <emscripten/emscripten.h>
+static std::vector<double> tmp_buffer;
 long double e = M_E;
-long double sigmoid(long double x) {
-    return 1.0L/(1.0L+pow(e, -x));
-}
-long double sigmoidDer(long double x) {
-    long double s = sigmoid(x);
-    return s * (1-s);
-}
-long double sum(std::vector<long double> vals, std::vector<long double> w, long double b) {
-    long double res = 0;
-    for (int i = 0; i < vals.size(); i++) {
-        res += vals[i]*w[i];
-    }
-    return res+b;
-}
+extern "C" {
+  long double sigmoid(long double x) {
+      return 1.0L/(1.0L+pow(e, -x));
+  }
+  long double sigmoidDer(long double x) {
+      long double s = sigmoid(x);
+      return s * (1-s);
+  }
+  long double sum(std::vector<long double> vals, std::vector<long double> w, long double b) {
+      long double res = 0;
+      for (int i = 0; i < vals.size(); i++) {
+          res += vals[i]*w[i];
+      }
+      return res+b;
+  }
 
-// last layer error
-std::vector<long double> LLerror(
-    std::vector<long double> hL,
-    std::vector<std::vector<long double>> &who,
-    int sample,
-    std::vector<std::vector<long double>> &y,
-    std::vector<long double> &oL_out,
-    std::vector<long double> b
-) {
-    std::vector<long double> deltaO(who.size());
+  // last layer error
+  std::vector<long double> LLerror(
+      std::vector<long double> hL,
+      std::vector<std::vector<long double>> &who,
+      int sample,
+      std::vector<std::vector<long double>> &y,
+      std::vector<long double> &oL_out,
+      std::vector<long double> b
+  ) {
+      std::vector<long double> deltaO(who.size());
 
-    for (int j = 0; j < who.size(); j++) {
-        long double z_out = sum(hL, who[j], b[j]);
-        oL_out[j] = sigmoid(z_out);
-        deltaO[j] = (oL_out[j] - y[sample][j]) * sigmoidDer(z_out);
-    }
-    return deltaO;
-}
+      for (int j = 0; j < who.size(); j++) {
+          long double z_out = sum(hL, who[j], b[j]);
+          oL_out[j] = sigmoid(z_out);
+          deltaO[j] = (oL_out[j] - y[sample][j]) * sigmoidDer(z_out);
+      }
+      return deltaO;
+  }
 
-// hidden layer error
-std::vector<long double> HLerror(
-    std::vector<long double> nextDelta,
-    std::vector<std::vector<long double>> &whn,
-    std::vector<long double> &hL
-) {
-    std::vector<long double> deltaH(hL.size());
+  // hidden layer error
+  std::vector<long double> HLerror(
+      std::vector<long double> nextDelta,
+      std::vector<std::vector<long double>> &whn,
+      std::vector<long double> &hL
+  ) {
+      std::vector<long double> deltaH(hL.size());
 
-    for (int i = 0; i < hL.size(); i++) {
-        long double sumErr = 0;
-        for (int j = 0; j < nextDelta.size(); j++) {
-            sumErr += whn[j][i] * nextDelta[j];
-        }
-        deltaH[i] = hL[i] * (1 - hL[i]) * sumErr;
-    }
-    return deltaH;
-}
+      for (int i = 0; i < hL.size(); i++) {
+          long double sumErr = 0;
+          for (int j = 0; j < nextDelta.size(); j++) {
+              sumErr += whn[j][i] * nextDelta[j];
+          }
+          deltaH[i] = hL[i] * (1 - hL[i]) * sumErr;
+      }
+      return deltaH;
+  }
 
-// update weights hidden->output
-std::vector<std::vector<long double>> OLupd(
-    std::vector<long double> &hL,
-    std::vector<std::vector<long double>> who,
-    std::vector<long double> &deltaO,
-    long double lr
-) {
-    for (int j = 0; j < who.size(); j++) {
-        for (int i = 0; i < hL.size(); i++) {
-            who[j][i] -= lr * deltaO[j] * hL[i];
-        }
-    }
-    return who;
-}
+  // update weights hidden->output
+  std::vector<std::vector<long double>> OLupd(
+      std::vector<long double> &hL,
+      std::vector<std::vector<long double>> who,
+      std::vector<long double> &deltaO,
+      long double lr
+  ) {
+      for (int j = 0; j < who.size(); j++) {
+          for (int i = 0; i < hL.size(); i++) {
+              who[j][i] -= lr * deltaO[j] * hL[i];
+          }
+      }
+      return who;
+  }
 
-// update weights input->hidden
-std::vector<std::vector<long double>> HLupd(
-    std::vector<long double> &iL,
-    std::vector<std::vector<long double>> wih,
-    std::vector<long double> &deltaH,
-    long double lr
-) {
-    for (int i = 0; i < wih.size(); i++) {
-        for (int k = 0; k < iL.size(); k++) {
-            wih[i][k] -= lr * deltaH[i] * iL[k];
-        }
+  // update weights input->hidden
+  std::vector<std::vector<long double>> HLupd(
+      std::vector<long double> &iL,
+      std::vector<std::vector<long double>> wih,
+      std::vector<long double> &deltaH,
+      long double lr
+  ) {
+      for (int i = 0; i < wih.size(); i++) {
+          for (int k = 0; k < iL.size(); k++) {
+              wih[i][k] -= lr * deltaH[i] * iL[k];
+          }
+      }
+      return wih;
+  }
+  std::vector<long double> Bupd(std::vector<long double> &b, std::vector<long double> &delta, long double lr) {
+      for (int i = 0; i < b.size(); i++) {
+          b[i] -= lr * delta[i];
+      }
+      return b;
+  }
+  long double sech(long double x) {return 1/cosh(x);}
+  long double ReLU(long double x) {
+    if (x > 0) {
+      return x;
+    } else {
+      return 0;
     }
-    return wih;
-}
-std::vector<long double> Bupd(std::vector<long double> &b, std::vector<long double> &delta, long double lr) {
-    for (int i = 0; i < b.size(); i++) {
-        b[i] -= lr * delta[i];
+  }
+  long double ReLUDer(long double x) {
+    if (x > 0) {
+      return 1;
+    } else {
+      return 0;
     }
-    return b;
-}
-long double sech(long double x) {return 1/cosh(x);}
-long double ReLU(long double x) {
+  }
+
+  long double tanhDer(long double x) {
+    return pow(sech(x), 2);
+  }
+
+  long double PReLU(long double x, long double a) {
   if (x > 0) {
-    return x;
-  } else {
-    return 0;
+      return x;
+    } else {
+      return a*x;
+    }
   }
-}
-long double ReLUDer(long double x) {
+
+  long double PReLUDer(long double x, long double a) {
   if (x > 0) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-long double tanhDer(long double x) {
-  return pow(sech(x), 2);
-}
-
-long double PReLU(long double x, long double a) {
-if (x > 0) {
-    return x;
-  } else {
-    return a*x;
-  }
-}
-
-long double PReLUDer(long double x, long double a) {
-if (x > 0) {
-    return 1;
-  } else {
-    return a;
+      return 1;
+    } else {
+      return a;
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,24 +1,50 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const ml = require("./../lib/MachineLearning-CppModule/ML.js");
+let x = [[0], [1]];
+let y = [[1], [0]];
+let i = [0];
+let h = [0, 0];
+let o = [0];
+let bh = [0, 0];
+let bo = [0];
+let wh = [[0], [0]];
+let wo = [[0, 0]];
+for (let i = 0; i < wh.length; i++) {
+  for (let n = 0; n < wh[i].length; n++) {
+    wh[i][n] = Math.random();
+  }
+}
+for (let i = 0; i < wo.length; i++) {
+  for (let n = 0; n < wo[i].length; n++) {
+    wo[i][n] = Math.random();
+  }
+}
+let lr = 0.1;
+let index = 0;
+let epochs = 50000;
+// Main loop
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+for (let epoch = 0; epoch < epochs; epoch++) {
+  index = epoch % 2;
+  iL = [x[index][0]];
+  for (let i = 0; i < h.length; i++) {
+    h[i] = ml.sigmoid(ml.sum(i, h[i], bh[i]));
+  }
+  eLL = ml.LLerror(h, wo, index, y, o, bo);
+  eLh = ml.HLerror(eLL, wo, h);
 
-setupCounter(document.querySelector('#counter'))
+  wo = ml.OLupd(h, wo, eLL, lr);
+  wh = ml.HLupd(i, wh, eLh, lr);
+  bh = ml.Bupd(bh, eLh, lr);
+  oh = ml.Bupd(oh, eLL, lr);
+}
+
+for (let i = 0; i < x.length(); i++) {
+  iL = [x[i][0]];
+  for (let n = 0; n < h.length(); n++) {
+    h[i] = ml.sigmoid(ml.sum(i, wh, bh));
+  }
+  for (let n = 0; n < o.length(); n++) {
+    o[i] = ml.sigmoid(ml.sum(i, wo, bo));
+  }
+  console.log(`x=${x[i][0]}, y=${y[i][0]}, ypred = ${o[0]}`);
+}
